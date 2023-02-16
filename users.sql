6a3c32cc7e6094d286a84418913046b7190e5d87
JesusAParedes <paredesjesus93@gmail.com>
2023-02-16 13:07:36 -0600

    rename initialize file and make a foodie schema sql file

---
+++ users.sql
@@ -0,0 +1,34 @@
+
+INSERT INTO users
+    (user_id, first_name, last_name, email)
+VALUES
+    (11, "John", "Jacob", "jj@example.com"),
+    (22, "Yu", "Kwon", "yk2@swan.org"),
+    (32, "Jane", "Stamos", "jstamos@thatplace.com"),
+    (42, "Hawk", "Griffin", "eagle@yahoo.com");
+
+INSERT INTO food
+    (food_id, user_id, food_name, rating)
+VALUES
+    (1, 11, "Big Carl", "2/5"), 
+    (2, 32, "Chicken Quesadilla", "3/5"), 
+    (3, 22, "Beef Fajita Tacos", "5/5"),
+    (4, 42, "Buldak", "5/5");
+
+INSERT INTO restaurants
+    (food_id, name, address, phone, hours)
+VALUES
+    (1, "Carl's Jr", "1250 Somewhere Ln", "800-966-2500", "9AM-9PM"),
+    (2, "Taco Bell", "999 Island Ave", "929-556-2746", "8AM-12AM"),
+    (3, "El Patron", "168 Lavender Valley", "222-666-4444", "6AM-7PM"),
+    (4, "SoHa", "892 Rocky Rd", "230-555-8888", "8AM-8PM");
+
+INSERT INTO userCredentials
+    (user_id, username, pw)
+VALUES
+    (11, "jjrocks", "jdaman2!"),
+    (22, "y2serious", "k8tie"),
+    (32, "jessesgirl", "G018T0"),
+    (42, "hawklanded", "!i9@l0"); 
+
+
