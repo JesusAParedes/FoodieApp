04f16c3e80ac478d5f64f984839db25c82ce08e0
JesusAParedes <paredesjesus93@gmail.com>
2023-02-21 20:07:57 -0600

    add basic layout to restaurant files

---
+++ controller/restaurant.js
@@ -0,0 +1,6 @@
+const mysql = require('mysql2');
+const pool = require('../mysql/connection.js');
+const { errors } = require('../mysql/errors.js');
+
+
+module.exports = {};
---
+++ routes/restaurant.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const restaurantController = require('../controller/restaurant.js');
+ 
+
+module.exports = router;
