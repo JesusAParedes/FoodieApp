ba0b3945866469693ad5f4430f55be2d27d08880
JesusAParedes <paredesjesus93@gmail.com>
2023-02-21 21:00:00 -0600

    start to look at how to add authentication for add user

--- controller/users.js
+++ controller/users.js
@@ -27,7 +27,7 @@ const userById = (req, res, next) => {
 };
 
 const addUser = (req, res, next) => {
-    if(!req.body) return res.sendStatus(400);
+    if(!req.body) return res.status(400);
 
     let sql = 'INSERT INTO ??(??, ??, ??, ??, ??) VALUES (?,?,?,?,?)';
     let rep = ['users', 'first_name', 'last_name', 'email', 'username', 'password', req.body.first_name, req.body.last_name, req.body.email, req.body.username, req.body.password];
