5be2def7d0aafdbc939adefde55825d5c91474af
JesusAParedes <paredesjesus93@gmail.com>
2023-05-03 22:08:29 -0500

    add middleware for authentication

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controller/users.js');
+const { checkJwt } = require('../middleware')
 
 router.get('/', usersController.defaultRoute);
 
@@ -11,12 +12,12 @@ router.get('/users', usersController.getAllUsers);
 router.get('/users/:id', usersController.userById);
 
 //create a new user
-router.post('/users', usersController.addUser);
+router.post('/users', checkJwt, usersController.addUser);
 
 //update a user's info
-router.put('/users/:id', usersController.updateUser);
+router.put('/users/:id', checkJwt, usersController.updateUser);
 
 //delete the user off the app
-router.delete('/users/:id', usersController.deleteUser);
+router.delete('/users/:id', checkJwt, usersController.deleteUser);
 
 module.exports = router;
