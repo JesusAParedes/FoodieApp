703d3d284ec2fd8b902631311ed6c7c54a51db9e
JesusAParedes <paredesjesus93@gmail.com>
2023-06-04 11:38:45 -0500

    readd cors package

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ app.use((req, res, next) => {
     next()
 })
 
-// app.use(cors());
+app.use(cors());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://foodie-app-react.vercel.app/"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "https://foodie-app-react.vercel.app/"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 
 app.use(express.json());
 
